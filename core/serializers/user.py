ca1d03067e2cc2f0519f1d4cf0b3ead37316acb7
kayquelopes <kayquelopes@gmail.com>
2025-09-15 08:59:11 -0300

    atualizado criptografia de senha

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -8,3 +8,11 @@ class UserSerializer(ModelSerializer):
         model = User
         fields = '__all__'
         depth = 1
+ 
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
